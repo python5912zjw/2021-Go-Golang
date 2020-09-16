ac12620f84a5aaca553ce7871384131669a51dfe
xiaolongtalkgo <xiaolongtalkgo@qq.com>
2020-09-17 00:18:53 +0800

    环境安装增加go mod module补充说明

--- 小龙说GO语言/01Go语言基础/环境安装/main.go
+++ 小龙说GO语言/01Go语言基础/环境安装/main.go
@@ -6,10 +6,15 @@ func main(){
 	fmt.Println("Hello World!")
 }
 
+//这个面向对象，先不用管，主要是我用来讲解go mod模式
+//小龙说GO语言文件夹其实就是一个module，
+//大家可以在cmd切换到小龙说GO语言文件夹目录执行go mod why
+//module是xiaolongtalkgo，这个是基准，以后所有的包引入都仰仗这个基准
 type Test struct{
 
 }
 
+//这个是面向对象，先不用管，主要是我用来讲解go mod模式
 func(test *Test) Test1(){
 	fmt.Println("test")
 }
--- 小龙说GO语言/01Go语言基础/环境安装/环境安装/test环境安装.go
+++ 小龙说GO语言/01Go语言基础/环境安装/环境安装/test环境安装.go
@@ -9,3 +9,7 @@ type Test struct{
 func(test *Test) Test1(){
 	fmt.Println("test")
 }
+
+func(test *Test) Test2(){
+	fmt.Println("GO语言环境安装文章:");
+}
