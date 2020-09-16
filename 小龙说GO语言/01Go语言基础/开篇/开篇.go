6b49f95d1f6a4757e7ff5467de91c2241dad8bdf
xiaolongtalkgo <xiaolongtalkgo@qq.com>
2020-09-16 22:23:29 +0800

    开篇

--- 小龙说GO语言/01Go语言基础/开篇/开篇.go
+++ 小龙说GO语言/01Go语言基础/开篇/开篇.go
@@ -7,7 +7,7 @@ type LongStruct struct{}
 func (longStruct *LongStruct) Begin() {
 	fmt.Println("Hello World!")
 	fmt.Println("GO擅长干的事情：WEB后端开发、Devops、微服务、运维开发、中间件、云原生")
-	fmt.Println("GO的优势：静态语言、天生并发、无依赖、C的速度python的开发效率")
+	fmt.Println("GO的优势：静态语言、天生并发、无依赖、C的运行速度python的开发效率")
 	fmt.Println("在职Golang工程师，GO后端开发一枚，利用业余时间录制GO就业班精品教程，" +
 		"不为任何机构站台，免费分享工作中实际用到的技术，能够助您入门到就业")
 	fmt.Println("热爱你的热爱，请点赞、评论、投币，转发，您的支持能让我更好的坚持下去");
--- 小龙说GO语言/小龙说GO语言.go
+++ 小龙说GO语言/小龙说GO语言.go
@@ -3,7 +3,11 @@ package main
 import (
 	"xiaolongtalkgo/01Go语言基础/开篇"
 )
+
 func main () {
+	//2021GO语言就业班视频正式开始录制
+	//讲师：小龙，在职Golang工程师，业余时间视频分享GO语言及后端相关技术
+	//讲课笔记：https://github.com/xiaolongtalkgo/2021-Go-Golang
 	var xiaolongtalkgo *开篇.LongStruct
 	xiaolongtalkgo.Begin();
 }
